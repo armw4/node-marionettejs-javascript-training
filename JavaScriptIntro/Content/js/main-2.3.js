f0aaa060b650efa24e3e80ef1dbd467c28abf281
armw4 <antwan.wimberly@gmail.com>
2013-04-09 15:43:23 -0400

    Add marionette to require dependancies.

--- JavaScriptIntro/Content/js/main-2.3.js
+++ JavaScriptIntro/Content/js/main-2.3.js
@@ -2,7 +2,8 @@
     paths: {
         'jquery': 'lib/jquery-1.9.1',
         'underscore': 'lib/underscore-1.4.4',
-        'backbone': 'lib/backbone-1.0.0'
+        'backbone': 'lib/backbone-1.0.0',
+        'marionette': 'lib/backbone.marionette-1.0.1'
     },
     shim: {
         'backbone': {
